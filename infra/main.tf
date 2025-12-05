10716eaa12a43379e8ccb76fa8bdbc5a5763e7cc
KjTejasvi <kjallamesh387@gmail.com>
2025-12-05 22:09:27 +0530

    main file completed

--- infra/main.tf
+++ infra/main.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "subnet_eks" {
     count = 2
     vpc_id = aws_vpc.vpc_eks.id
     cidr_block = cidrsubnet(aws_vpc.vpc_eks.cidr_block, 8, count.index)
-    availability_zone = element (["ap-south-1a", "ap-south-1b"], count.index)
+    availability_zone = element (["ap-south-1a","ap-south-1b"], count.index)
     map_public_ip_on_launch = true
     tags = {
         Name = "subnet_eks_${count.index}"
@@ -31,8 +31,8 @@ resource "aws_internet_gateway" "igw_eks" {
 #route table
 resource "aws_route_table" "rt_eks" {
     vpc_id = aws_vpc.vpc_eks.id
-    route = {
-        cide_block = "0.0.0.0/0"
+    route {
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.igw_eks.id
     }
     tags = {
@@ -53,7 +53,7 @@ resource "aws_security_group" "eks_cluster_sg" {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
         Name = "eks_cluster_sg"
@@ -66,13 +66,13 @@ resource "aws_security_group" "eks_node_sg" {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
         Name = "eks_node_sg"
@@ -110,7 +110,7 @@ resource "aws_iam_role" "eks_iam_node_role" {
         {
           "Effect": "Allow",
           "Principal": {
-            "Service": "eks.amazonaws.com"
+            "Service": "ec2.amazonaws.com"
           },
           "Action": "sts:AssumeRole"
         }
@@ -120,7 +120,7 @@ resource "aws_iam_role" "eks_iam_node_role" {
 }
 resource "aws_iam_role_policy_attachment" "eks_node_role_policy" {
     role = aws_iam_role.eks_iam_node_role.name
-    policy_arn = "are:aws:iam::aws/policy/AmazonEKSWorkerNodePolicy"
+    policy_arn = "arn:aws:iam::aws/policy/AmazonEKSWorkerNodePolicy"
 }
 resource  "aws_iam_role_policy_attachment" "eks_node_group_cni_policy" {
     role = aws_iam_role.eks_node_role.name
@@ -131,5 +131,30 @@ resource  "aws_iam_role_policy_attachment" "eks_node_group_registry_policy" {
     policy_arn = "arn:aws:iam::aws/policy/AmazonEC2ContainerRegistryReadOnly"
 }
 #eks cluster
+resource "aws_eks_cluster" "eks_cluster" {
+    name = "eks_cluster"
+    role_arn = aws_iam_role.eks_iam_cluster_role.arn
+    vpc_config {
+        subnet_ids = aws_subnet.subnet_eks[*].id
+        security_group_ids = [aws_security_group.eks_cluster_sg.id]
+    }
+}
+#eks node group
+resource "aws_eks_node_group" "eks_cluster" {
+    cluster_name = aws_eks_cluster.eks_cluster.name
+    node_group_name = "eks_node_group"
+    node_role_arn = aws_iam_role.eks_iam_node_role.arn
+    subnet_ids = aws_subnet.subnet_eks[*].id
+    scaling_config {
+        desired_size = 3
+        max_size = 5
+        min_size = 3
+    }
 
+    instance_types = ["t2.large"]
+    remote_access {
+        ec2_ssh_key = var.ec2_ssh_key_name
+        source_security_group_ids = [aws_security_group.eks_node_sg.id]
+    }
+}
 
