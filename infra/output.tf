315ab51488b881f4d6215680f416c5c5600aef34
KjTejasvi <kjallamesh387@gmail.com>
2025-12-08 20:41:26 +0530

    cluster code completed

---
+++ infra/output.tf
@@ -0,0 +1,12 @@
+output "Cluster_ID" {
+    description = "The ID of the created cluster"
+    value       = aws_eks_cluster.eks_cluster.id
+}
+output "node_group_name" {
+    description = "The name of the created node group"
+    value       = aws_eks_node_group.eks_cluster.id
+}
+output "vpc_id" {
+    description = "The ID of the created VPC"
+    value       = aws_vpc.vpc_eks.id
+}
